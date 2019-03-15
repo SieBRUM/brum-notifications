2f45d534acab11ea8098383220efdde281b31342
siebrum <siebrenkraak@hotmail.com>
2019-03-15 08:34:42 +0100

    Initial commit

---
+++ BrumAlert.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace BrumCustomAlerts
+{
+    partial class BrumAlert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerClose = new System.Windows.Forms.Timer(this.components);
+            this.timerShow = new System.Windows.Forms.Timer(this.components);
+            this.timerClosing = new System.Windows.Forms.Timer(this.components);
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerClose
+            // 
+            this.timerClose.Tick += new System.EventHandler(this.timerClose_Tick);
+            // 
+            // timerShow
+            // 
+            this.timerShow.Enabled = true;
+            this.timerShow.Interval = 1;
+            this.timerShow.Tick += new System.EventHandler(this.timerShow_Tick);
+            // 
+            // timerClosing
+            // 
+            this.timerClosing.Interval = 1;
+            this.timerClosing.Tick += new System.EventHandler(this.timerClosing_Tick);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblMessage.Location = new System.Drawing.Point(55, 19);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(61, 22);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "label1";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.Crimson;
+            this.btnClose.Image = global::BrumCustomAlerts.Properties.Resources.clear_mini_white;
+            this.btnClose.Location = new System.Drawing.Point(563, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(25, 25);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.OnClose);
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(12, 12);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(37, 36);
+            this.pbMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            // 
+            // BrumAlert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(594, 60);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.pbMain);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BrumAlert";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "alert";
+            this.Load += new System.EventHandler(this.alert_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Timer timerClose;
+        internal System.Windows.Forms.Timer timerShow;
+        internal System.Windows.Forms.Timer timerClosing;
+        internal System.Windows.Forms.PictureBox pbMain;
+        internal System.Windows.Forms.Label lblMessage;
+        internal System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ BrumAlert.cs
@@ -0,0 +1,266 @@
+﻿using BrumCustomAlerts.Properties;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace BrumCustomAlerts
+{
+    internal partial class BrumAlert : Form
+    {
+        int interval = 0;
+        AlertLocation alertLocation;
+
+        #region constructors
+        public BrumAlert(string message, AlertType type, int delay, AlertLocation alertLocation)
+        {
+            InitializeComponent();
+            lblMessage.Text = message;
+            this.alertLocation = alertLocation;
+
+            switch (type)
+            {
+                case AlertType.Success:
+                    pbMain.Image = Resources.success_white;
+                    BackColor = Color.LimeGreen;
+                    ForeColor = Color.White;
+                    lblMessage.ForeColor = Color.White;
+                    break;
+                case AlertType.Info:
+                    pbMain.Image = Resources.info_white;
+                    BackColor = Color.DeepSkyBlue;
+                    ForeColor = Color.White;
+                    lblMessage.ForeColor = Color.White;
+                    break;
+                case AlertType.Warning:
+                    pbMain.Image = Resources.warning_white;
+                    BackColor = Color.Orange;
+                    ForeColor = Color.White;
+                    lblMessage.ForeColor = Color.White;
+                    break;
+                case AlertType.Error:
+                    pbMain.Image = Resources.error_white;
+                    BackColor = Color.Crimson;
+                    ForeColor = Color.WhiteSmoke;
+                    lblMessage.ForeColor = Color.White;
+                    break;
+                default:
+                    throw new Exception("Unkown setting!");
+            }
+
+            Show();
+            timerClose.Interval = delay;
+            timerClose.Start();
+        }
+
+        public BrumAlert(string message, Color foregroundColor, Color backgroundColor, AlertType type, int delay, AlertLocation alertLocation)
+        {
+            InitializeComponent();
+            lblMessage.Text = message;
+            this.alertLocation = alertLocation;
+
+            switch (type)
+            {
+                case AlertType.Success:
+                    pbMain.Image = Resources.success_white;
+                    break;
+                case AlertType.Info:
+                    pbMain.Image = Resources.info_white;
+                    break;
+                case AlertType.Warning:
+                    pbMain.Image = Resources.warning_white;
+                    break;
+                case AlertType.Error:
+                    pbMain.Image = Resources.error_white;
+                    break;
+                default:
+                    throw new Exception("Unkown setting!");
+            }
+            BackColor = backgroundColor;
+            lblMessage.ForeColor = foregroundColor;
+
+            Show();
+            timerClose.Interval = delay;
+            timerClose.Start();
+        }
+
+        public BrumAlert(string message, Color foregroundColor, Color backgroundColor, Bitmap image, int delay, AlertLocation alertLocation)
+        {
+            InitializeComponent();
+            lblMessage.Text = message;
+            this.alertLocation = alertLocation;
+
+            pbMain.Image = image;
+            BackColor = backgroundColor;
+            lblMessage.ForeColor = foregroundColor;
+
+            Show();
+            timerClose.Interval = delay;
+            timerClose.Start();
+        }
+
+        #endregion
+        private void alert_Load(object sender, EventArgs e)
+        {
+            switch (alertLocation)
+            {
+                case AlertLocation.TopRight:
+                    Top = 0 - this.Height - 50;
+                    Left = Screen.PrimaryScreen.Bounds.Width - this.Width - 50;
+                    break;
+                case AlertLocation.TopMiddle:
+                    Top = 0 - this.Height - 50;
+                    Left = (Screen.PrimaryScreen.Bounds.Width / 2) - (Width / 2);
+                    break;
+                case AlertLocation.TopLeft:
+                    Top = 0 - this.Height - 50;
+                    Left = 50;
+                    break;
+                case AlertLocation.BottomRight:
+                    Top = Screen.PrimaryScreen.Bounds.Height;
+                    Left = Screen.PrimaryScreen.Bounds.Width - this.Width - 50;
+                    break;
+                case AlertLocation.BottomMiddle:
+                    Top = Screen.PrimaryScreen.Bounds.Height;
+                    Left = (Screen.PrimaryScreen.Bounds.Width / 2) - (Width / 2);
+                    break;
+                case AlertLocation.BottomLeft:
+                    Top = Screen.PrimaryScreen.Bounds.Height;
+                    Left = 50;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void OnClose(object sender, EventArgs e)
+        {
+            timerClosing.Start();
+        }
+
+        private void timerClose_Tick(object sender, EventArgs e)
+        {
+            timerClosing.Start();
+        }
+
+        private void timerShow_Tick(object sender, EventArgs e)
+        {
+            if (alertLocation == AlertLocation.TopLeft || alertLocation == AlertLocation.TopRight || alertLocation == AlertLocation.TopMiddle)
+            {
+                if (Top < this.Height - 30)
+                {
+                    Top += interval;
+                    interval += 1;
+                }
+                else
+                {
+                    timerShow.Stop();
+                }
+            }
+            else
+            {
+                if (Top > Screen.PrimaryScreen.Bounds.Height - this.Height - 55)
+                {
+                    Top -= interval;
+                    interval += 2;
+                }
+                else
+                {
+                    timerShow.Stop();
+                }
+            }
+        }
+
+        private void timerClosing_Tick(object sender, EventArgs e)
+        {
+            if (Opacity > 0)
+            {
+                Opacity -= 0.1;
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        #region Adding TOPMOST without stealing focus
+        protected override bool ShowWithoutActivation
+        {
+            get { return true; }
+        }
+
+        private const int WS_EX_TOPMOST = 0x00000008;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams createParams = base.CreateParams;
+                createParams.ExStyle |= WS_EX_TOPMOST;
+                createParams.ExStyle |= 0x80;
+                return createParams;
+            }
+        }
+        #endregion
+    }
+
+    public class BrumAlertFactory
+    {
+        /// <summary>
+        /// Default alert factory. 
+        /// </summary>
+        /// <param name="message">Alert message text</param>
+        /// <param name="type">Alert type</param>
+        /// <param name="delay">Delay in miliseconds. Default 2000 miliseconds</param>
+        /// <param name="alertLocation">Alert location. Default BottomRight</param>
+        public static void OpenAlert(string message, AlertType type, int delay = 2000, AlertLocation alertLocation = AlertLocation.BottomRight)
+        {
+            BrumAlert alert = new BrumAlert(message, type, delay, alertLocation);
+        }
+        /// <summary>
+        /// Open custom made alert with custom image
+        /// </summary>
+        /// <param name="message">Alert message text</param>
+        /// <param name="foregroundColor">Text color</param>
+        /// <param name="backgroundColor">Background color</param>
+        /// <param name="image">Alert icon</param>
+        /// <param name="delay">Delay in miliseconds. Default 2000 miliseconds</param>
+        /// <param name="alertLocation">Alert location. Default BottomRight</param>
+        public static void OpenAlert(string message, Color foregroundColor, Color backgroundColor, Bitmap image, int delay = 2000, AlertLocation alertLocation = AlertLocation.BottomRight)
+        {
+            BrumAlert alert = new BrumAlert(message, foregroundColor, backgroundColor, image, delay, alertLocation);
+        }
+
+        /// <summary>
+        /// Open custom alert with pre-set icon
+        /// </summary>
+        /// <param name="message">Alert message text</param>
+        /// <param name="foregroundColor">Text color</param>
+        /// <param name="backgroundColor">Background color</param>
+        /// <param name="type">Alert type for pre-set image</param>
+        /// <param name="delay">Delay in miliseconds. Default 2000 miliseconds</param>
+        /// <param name="alertLocation"Alert location. Default BottomRight></param>
+        public static void OpenAlert(string message, Color foregroundColor, Color backgroundColor, AlertType type, int delay = 2000, AlertLocation alertLocation = AlertLocation.BottomRight)
+        {
+            BrumAlert alert = new BrumAlert(message, foregroundColor, backgroundColor, type, delay, alertLocation);
+        }
+    }
+
+
+    public enum AlertType
+    {
+        Success,
+        Info,
+        Warning,
+        Error
+    }
+
+    public enum AlertLocation
+    {
+        TopRight,
+        TopMiddle,
+        TopLeft,
+        BottomRight,
+        BottomMiddle,
+        BottomLeft
+    }
+}
